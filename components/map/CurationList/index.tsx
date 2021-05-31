302721956316ece5b9ca5212d0e8b09af2b76eb5
pa-rang <appdler96@kaist.ac.kr>
2021-05-31 19:29:58 +0900

    feat(Map): add overflow scroll to curation list

--- components/map/CurationList/index.tsx
+++ components/map/CurationList/index.tsx
@@ -7,13 +7,18 @@ const Styled = {
   Root: styled.div`
     position: absolute;
     top: 72px;
+    bottom: 72px;
+    overflow-x: scroll;
     right: 40px;
     z-index: 999;
   `,
 };
 
 function CurationList() {
-  const { data: curations } = useGetCuration();
+  const { data } = useGetCuration();
+
+  /** FIX: Curation list가 화면을 넘어갈 때 테스트용 */
+  const curations = data?.concat(data);
 
   return (
     <Styled.Root>
