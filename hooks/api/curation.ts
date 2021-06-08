3292c3f1164035e86ec0df4f97653b262078ecf0
pa-rang <appdler96@kaist.ac.kr>
2021-06-08 19:27:43 +0900

    chore: fix api response type

--- hooks/api/curation.ts
+++ hooks/api/curation.ts
@@ -2,7 +2,11 @@ import useSWR from "swr";
 import http from "../../lib/api";
 import { ICuration } from "../../types";
 
+interface GetCuration {
+  curations: ICuration[];
+}
+
 export const useGetCuration = () => {
-  const { data, error } = useSWR("/curation", (url) => http.get<{ curations: ICuration[] }>(url));
+  const { data, error } = useSWR("/curation", (url) => http.get<GetCuration>(url));
   return { data: data?.curations, error };
 };
--- hooks/api/restaurant.ts
+++ hooks/api/restaurant.ts
@@ -2,7 +2,11 @@ import useSWR from "swr";
 import http from "../../lib/api";
 import { IRestaurant } from "../../types";
 
+interface GetRestaurant {
+  restaurants: IRestaurant[];
+}
+
 export const useGetRestaurant = () => {
-  const { data, error } = useSWR("/restaurant", (url) => http.get<{ restaurants: IRestaurant[] }>(url));
+  const { data, error } = useSWR("/restaurant", (url) => http.get<GetRestaurant>(url));
   return { data: data?.restaurants, error };
 };
