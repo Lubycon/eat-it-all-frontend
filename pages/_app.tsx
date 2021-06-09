2cf090a7e1ef9d6decec630e670ef3515b42c526
pa-rang <appdler96@kaist.ac.kr>
2021-06-09 20:07:12 +0900

    style: add reset-css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/contentLayout.css";
+import "reset-css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
