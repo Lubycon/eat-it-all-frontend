adcb2f0401aa6b0ef1d1fc67ca4a29a9fd710321
pa-rang <appdler96@kaist.ac.kr>
2021-06-09 19:40:49 +0900

    chore: add clickable css to go-to-map btn

--- components/common/GoToMapButton.tsx
+++ components/common/GoToMapButton.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Link from "next/link";
 import styled from "@emotion/styled";
 import { colors } from "../../lib/constants/colors";
+import mixin from "../../lib/style/mixin";
 
 const Styled = {
   Root: styled.img`
     border-radius: 5px;
     background-color: ${colors.beige20};
+    ${mixin.clickable};
 
     &:hover {
       box-shadow: 0px 4px 8px rgba(155, 110, 69, 0.25);
