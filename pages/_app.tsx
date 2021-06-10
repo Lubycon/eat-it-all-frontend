11ef3e048e1f5103876ee17fdd1c50b6c766fa32
pa-rang <appdler96@kaist.ac.kr>
2021-06-10 21:28:36 +0900

    dev: add slick-carousel for slick style css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "reset-css";
 import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/contentLayout.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
