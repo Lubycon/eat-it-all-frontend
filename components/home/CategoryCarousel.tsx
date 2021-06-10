db264a0a9d7ae5b5924b7c46c7fdf0c7298f9ff3
pa-rang <appdler96@kaist.ac.kr>
2021-06-10 21:39:19 +0900

    feat: add slider props

--- components/home/CategoryCarousel.tsx
+++ components/home/CategoryCarousel.tsx
@@ -2,53 +2,83 @@ import React from "react";
 import styled from "@emotion/styled";
 import Carousel from "../common/Carousel";
 import { colors } from "../../lib/constants/colors";
-import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider, { Settings } from "react-slick";
 
 const Styled = {
   Root: styled.div`
-    width: 100%;
+    /* width: 100%; */
   `,
 
   Title: styled.div`
     font-weight: bold;
     font-size: 18px;
+
     color: ${colors.beige30};
     padding: 10px;
     margin-bottom: 18px;
   `,
+
+  SliderItem: styled.div`
+    height: 200px;
+    padding: 16px;
+    background-color: #fff;
+    border: 1px solid red;
+    font-size: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+};
+
+const sliderProps: Settings = {
+  arrows: true,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 
 function CategoryCarousel() {
-  const sliderProps = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
     <div>
       <Styled.Title>Category</Styled.Title>
       <Slider {...sliderProps}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        <Styled.SliderItem>1</Styled.SliderItem>
+        <Styled.SliderItem>2</Styled.SliderItem>
+        <Styled.SliderItem>3</Styled.SliderItem>
+        <Styled.SliderItem>4</Styled.SliderItem>
+        <Styled.SliderItem>5</Styled.SliderItem>
+        <Styled.SliderItem>6</Styled.SliderItem>
       </Slider>
     </div>
   );
