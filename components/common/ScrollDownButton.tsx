393777e1a24cb3325bcf67951beeb1afa847a723
pa-rang <appdler96@kaist.ac.kr>
2021-06-13 23:14:08 +0900

    chore: add scroll down icons

--- components/common/ScrollDownButton.tsx
+++ components/common/ScrollDownButton.tsx
@@ -1,7 +1,17 @@
+import styled from "@emotion/styled";
 import React from "react";
+import mixin from "../../lib/style/mixin";
+
+const Styled = {
+  Img: styled.img`
+    position: absolute;
+    bottom: 20px;
+    ${mixin.clickable}
+  `,
+};
 
 function ScrollDownButton() {
-  return <div>ScrollDownButton</div>;
+  return <Styled.Img src="/assets/icons/scroll_down.svg" alt="scroll-down" />;
 }
 
 export default ScrollDownButton;
---
+++ hooks/useWindowSize.ts
@@ -0,0 +1,31 @@
+import React from "react";
+
+function useWindowSize() {
+  const isClient = typeof window === "object";
+
+  const getSize = () => {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
+    };
+  };
+
+  const [windowSize, setWindowSize] = React.useState(getSize);
+
+  React.useEffect(() => {
+    if (!isClient) {
+      return;
+    }
+
+    const handleResize = () => {
+      setWindowSize(getSize());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
+
+export default useWindowSize;
