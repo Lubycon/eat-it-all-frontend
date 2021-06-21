02d4722da3bb39d2105cf8ae806abd7edc4206aa
pa-rang <appdler96@kaist.ac.kr>
2021-06-21 18:25:16 +0900

    style(Curation): add description and contents layout

--- pages/curation/[id].tsx
+++ pages/curation/[id].tsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import { useRouter } from "next/dist/client/router";
 import React from "react";
 import { useGetCuration } from "../../hooks/api/curation";
+import { colors } from "../../lib/constants/colors";
 
 const Styled = {
   Header: styled.div<{ image?: string }>`
@@ -17,11 +18,33 @@ const Styled = {
     display: flex;
     justify-content: center;
     align-items: flex-end;
+
+    @media (max-width: 768px) {
+      font-size: 32px;
+      height: 240px;
+    }
   `,
 
   CurationTitle: styled.div`
-    width: calc(600px + 32px);
-    margin: 0 16px 24px 16px;
+    width: 600px;
+    margin: 0 32px 24px 32px;
+  `,
+
+  CurationContent: styled.div`
+    position: relative;
+    max-width: calc(600px + 64px);
+    padding: 56px 32px;
+    margin: 0 auto;
+
+    .description {
+      font-size: 18px;
+      line-height: 1.8;
+      color: ${colors.gray90};
+    }
+  `,
+
+  CurationDescription: styled.div`
+    margin-bottom: 88px;
   `,
 };
 
@@ -31,12 +54,23 @@ function Curation() {
   } = useRouter();
 
   const { data: curation } = useGetCuration(Number(curationId));
+  console.log(`curation`, curation);
+
+  if (curation == null) return <div>Loading...</div>;
 
   return (
     <div>
-      <Styled.Header image={`https://file.eat-all.io${curation?.imageUrl}`}>
-        <Styled.CurationTitle>{curation?.title}</Styled.CurationTitle>
+      <Styled.Header image={`https://file.eat-all.io${curation.imageUrl}`}>
+        <Styled.CurationTitle>{curation.title}</Styled.CurationTitle>
       </Styled.Header>
+      <Styled.CurationContent>
+        <Styled.CurationDescription className="description">
+          모밀은 메밀로 만들어져 저칼로리 음식의 대표주자 입니다. 쏼라쏼라 쏼라쏼라 쏼라쏼라 쏼라쏼라 쏼라쏼라 쏼라쏼라
+          쏼라쏼라 모밀은 메밀로 만들어져 저칼로리 음식의 대표주자 입니다. 쏼라쏼라 쏼라쏼라 쏼라쏼라 쏼라쏼라 쏼라쏼라
+          쏼라쏼라 쏼라쏼라 모밀은 메밀로 만들어져 저칼로리 음식의 대표주자 입니다. 쏼라쏼라 쏼라쏼라 쏼라쏼라 쏼라쏼라
+          쏼라쏼라 쏼라쏼라 쏼라쏼라 모밀은 메밀로 만들어져 저칼로리 음식의 대표주자 입니다.
+        </Styled.CurationDescription>
+      </Styled.CurationContent>
       <div style={{ height: "2400px" }} />
     </div>
   );
