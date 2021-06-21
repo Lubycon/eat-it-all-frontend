b0103b74d1f16fb8452616be2dc6a62c498b5bd2
pa-rang <appdler96@kaist.ac.kr>
2021-06-21 18:38:25 +0900

    type: fix restaurant type interface

--- types/index.ts
+++ types/index.ts
@@ -9,11 +9,14 @@ export interface IRestaurant {
     latitude: number;
     longitude: number;
   };
+  description: string;
   curationIds: string[];
 }
 
 export interface ICuration {
+  content: string;
   id: number;
   title: string;
   imageUrl: string;
+  restaurants: IRestaurant[];
 }
