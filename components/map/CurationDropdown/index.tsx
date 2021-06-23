b5e72beed123194e536d57f0f0602cf702efdece
pa-rang <appdler96@kaist.ac.kr>
2021-06-23 20:44:35 +0900

    feat(Map): add dropdown menu component

--- components/map/CurationDropdown/index.tsx
+++ components/map/CurationDropdown/index.tsx
@@ -10,6 +10,9 @@ const Styled = {
     top: 36px;
     left: calc(50% - 200px);
     z-index: 10;
+  `,
+
+  DropdownHeader: styled.div`
     width: 400px;
     background: ${colors.white};
     box-shadow: 0px 3px 15px rgba(79, 62, 43, 0.45);
@@ -22,12 +25,20 @@ const Styled = {
     justify-content: space-between;
     align-items: center;
     border: 2px solid ${colors.white};
-    transition: 0.2s;
+    transition: 0.2s ease-in-out;
     ${clickable}
 
     &:hover {
       border: 2px solid ${colors.green40};
     }
+  `,
+
+  DropdownMenu: styled.div`
+    position: absolute;
+    top: 66px;
+    width: 400px;
+    height: 400px;
+    background: ${colors.white};
   `,
 
   CurationLeft: styled.div`
@@ -38,10 +49,16 @@ const Styled = {
       margin-right: 4px;
     }
   `,
+
+  ArrowIcon: styled.img<{ isRotate: boolean }>`
+    transition: 0.2s ease-in-out;
+    transform: rotate(${({ isRotate }) => (isRotate ? "0.5turn" : "0turn")});
+  `,
 };
 
 function CurationDropdown() {
   const { data: curations } = useGetCurations();
+  const [openDropdown, setOpenDropdown] = React.useState(false);
 
   if (curations == null) return <div>Loading...</div>;
 
@@ -49,11 +66,14 @@ function CurationDropdown() {
 
   return (
     <Styled.Root>
-      <Styled.CurationLeft>
-        <img src="/assets/icons/ic_curation_star.svg" />
-        <div>{dropdownItem[1]}</div>
-      </Styled.CurationLeft>
-      <img src="/assets/icons/ic_dropdown.svg" />
+      <Styled.DropdownHeader onClick={() => setOpenDropdown((prevState) => !prevState)}>
+        <Styled.CurationLeft>
+          <img src="/assets/icons/ic_curation_star.svg" />
+          <div>{dropdownItem[1]}</div>
+        </Styled.CurationLeft>
+        <Styled.ArrowIcon src="/assets/icons/ic_dropdown.svg" isRotate={openDropdown} />
+      </Styled.DropdownHeader>
+      {openDropdown && <Styled.DropdownMenu></Styled.DropdownMenu>}
     </Styled.Root>
   );
 }
