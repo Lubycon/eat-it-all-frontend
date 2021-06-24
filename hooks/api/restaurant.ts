317eaec9c1d6ded5b09622c7a71438213c3aaa3b
pa-rang <appdler96@kaist.ac.kr>
2021-06-25 01:29:19 +0900

    feat: add get one restaurant api

--- hooks/api/restaurant.ts
+++ hooks/api/restaurant.ts
@@ -2,11 +2,20 @@ import useSWR from "swr";
 import http from "../../lib/api";
 import { Restaurant } from "../../types";
 
-interface GetRestaurant {
+interface GetRestaurants {
   restaurants: Restaurant[];
 }
 
+interface GetRestaurant {
+  restaurant: Restaurant;
+}
+
 export const useGetRestaurants = () => {
-  const { data, error } = useSWR("/restaurant", (url) => http.get<GetRestaurant>(url));
+  const { data, error } = useSWR("/restaurant", (url) => http.get<GetRestaurants>(url));
   return { data: data?.restaurants, error };
+};
+
+export const useGetRestaurant = (id: number) => {
+  const { data, error } = useSWR(`/restaurant/${id}`, (url) => http.get<GetRestaurant>(url));
+  return { data: data?.restaurant, error };
 };
