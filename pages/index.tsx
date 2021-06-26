ba04c15f3e6300bfe6e6299cedf62e3b3a1d8e34
pa-rang <appdler96@kaist.ac.kr>
2021-06-26 12:13:02 +0900

    fix: 홈화면 진입 시 모달 state 초기화

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Main from "../components/home/Main";
 import Header from "../components/common/Header";
-import { colors } from "../lib/constants/colors";
 import MobileCategoryList from "../components/home/MobileCategoryList";
 import { useMobile } from "../hooks/useMobile";
+import { useSetRecoilState } from "recoil";
+import { modalRestaurantIdState } from "../store";
 
 function Home() {
   const isMobile = useMobile();
+  const setModalRestaurantId = useSetRecoilState(modalRestaurantIdState);
+
+  React.useEffect(() => {
+    setModalRestaurantId(null);
+  }, []);
 
   return (
     <>
