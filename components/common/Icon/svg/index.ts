4d52e63c457489c045856986600755fd26180c7b
pa-rang <appdler96@kaist.ac.kr>
2021-08-15 15:02:47 +0900

    feat: migrate icons to icon component

--- components/common/Icon/svg/index.ts
+++ components/common/Icon/svg/index.ts
@@ -1,3 +1,25 @@
+import { ReactComponent as ic_arrow } from './ic_arrow.svg';
+import { ReactComponent as ic_back_white } from './ic_back_white.svg';
+import { ReactComponent as ic_cancel } from './ic_cancel.svg';
+import { ReactComponent as ic_curation_star } from './ic_curation_star.svg';
+import { ReactComponent as ic_dropdown } from './ic_dropdown.svg';
+import { ReactComponent as ic_map_back } from './ic_map_back.svg';
+import { ReactComponent as ic_map_white } from './ic_map_white.svg';
 import { ReactComponent as logo } from './logo.svg';
+import { ReactComponent as map_ivory } from './map_ivory.svg';
+import { ReactComponent as marker } from './marker.svg';
+import { ReactComponent as scroll_down } from './scroll_down.svg';
 
-export { logo };
+export {
+  ic_arrow,
+  ic_back_white,
+  ic_cancel,
+  ic_curation_star,
+  ic_dropdown,
+  ic_map_back,
+  ic_map_white,
+  logo,
+  map_ivory,
+  marker,
+  scroll_down,
+};
